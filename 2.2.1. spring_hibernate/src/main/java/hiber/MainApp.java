17089972205f14e89d0de53ced8fa7157884c238
a11igatornn <cgs-nn@mail.ru>
2022-05-26 21:27:58 +0300

    commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,10 +1,14 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.dao.UserDao;
+import hiber.dao.UserDaoImp;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,10 +19,31 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+//
+//      List<User> users = userService.listUsers();
+//      for (User user : users) {
+//         System.out.println("Id = "+user.getId());
+//         System.out.println("First Name = "+user.getFirstName());
+//         System.out.println("Last Name = "+user.getLastName());
+//         System.out.println("Email = "+user.getEmail());
+//         System.out.println();
+//      }
+
+      User user1 = new User("User1", "LastName1", "User1@mail.ru");
+      User user2 = new User("User2", "LastName2", "User2@mail.ru");
+
+      Car car1 = new Car("Car1", 111);
+      Car car2 = new Car("Car2", 222);
+
+      user1.setCar(car1);
+      user2.setCar(car2);
+
+      userService.add(user1);
+      userService.add(user2);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,7 +51,17 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Model = "+user.getCar().getModel());
+         System.out.println("Series = "+user.getCar().getSeries());
          System.out.println();
+      }
+
+//      System.out.println(userService.getCarUser("Car1", 11));
+      try {
+         User carUser = userService.getCarUser("Car1", 11);
+         System.out.println(carUser);
+      } catch (NoResultException e) {
+         System.out.println("User отсутствует");
       }
 
       context.close();
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -19,6 +19,19 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne
+   @MapsId
+   @JoinColumn
+   private Car car;
+
+   public void setCar(Car car) {
+      this.car = car;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
    public User() {}
    
    public User(String firstName, String lastName, String email) {
@@ -57,5 +70,16 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", car=" + car +
+              '}';
    }
 }
